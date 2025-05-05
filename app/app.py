a55236abbd5fa656291ef4687e2ba4aa9c12c695
remots22 <remo.tsernant.rt@gmail.com>
2025-05-05 19:07:05 +0300

    Esimene töötav versioon kaardirakendusest leafleti abil. Lisatud külgriba ja kihtide sisse-välja lülitamine. hetkel veel ainult Tallinnal üks töötav kiht. peab QGIS-is hiljem neid juurde tegema

--- app/app.py
+++
@@ -1,6 +0,0 @@
-# meeldetuletus - vaadata kas pigem folium v streamlit - kui rohkem kasutajaid v private repo jms
-# ideaalis mihkliga miski muu js kui jouame
-import streamlit as st
-
-st.title("Test")
-st.write("test")
