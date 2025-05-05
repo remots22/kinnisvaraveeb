a10bb50f14400cf3f0a4064dffbe833bc58b4009
remots22 <remo.tsernant.rt@gmail.com>
2025-05-05 13:07:26 +0300

    Streamlit test

---
+++ app/app.py
@@ -0,0 +1,6 @@
+# meeldetuletus - vaadata kas pigem folium v streamlit - kui rohkem kasutajaid v private repo jms
+# ideaalis mihkliga miski muu js kui jouame
+import streamlit as st
+
+st.title("Test")
+st.write("test")
